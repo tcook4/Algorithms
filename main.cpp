493f7fd04bdd45a5a7a6c0d567985708907db610
Tyler Cook <tyler.b.cook4@gmail.com>
2018-01-19 23:31:09 -0600

    Added todo

--- main.cpp
+++ main.cpp
@@ -7,3 +7,19 @@ int main()
     cout << "Hello World!" << endl;
     return 0;
 }
+
+
+/* TODO
+ * -// sorts
+ -void bubbleSort();
+ -void selectionSort();
+ -void insertionSort();
+ -void mergeSort();
+ -void quickSort();
+ -void heapSort();
+ -
+ -// algorithms
+ -void djkstrasAlgo();
+ -void primsAlgo();
+
+ */
